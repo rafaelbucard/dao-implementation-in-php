64769d738f7ef3bbd5897ed33ec737775960735c
Rafael Pereira Buçard <36693529+rafaelbucard@users.noreply.github.com>
2021-05-29 22:39:23 -0300

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,12 +1,8 @@
 <?php
 class Usuario {
     private $id;
-    private $name;
-    private $client;
-    private $price;
-    private $desc;
-    private $date;
-    private $status;
+    private $nome;
+    private $email;
 
     public function getId() {
         return $this->id;
@@ -15,54 +11,25 @@ class Usuario {
         $this->id = trim($i);
     }
 
-    public function getName() {
-        return $this->name;
+    public function getNome() {
+        return $this->nome;
     }
-    public function setName($n) {
-        $this->name = ucwords($n);
-    }
-
-
-    public function getClient() {
-        return $this->client;
-    }
-    public function setClient($c) {
-        $this->client= ucwords($c);
+    public function setNome($n) {
+        $this->nome = ucwords(trim($n));
     }
 
-    public function getPrice() {
-        return $this->price;
+    public function getEmail() {
+        return $this->email;
     }
-    public function setPrice($p) {
-        $this->price = $p;
+    public function setEmail($e) {
+        $this->email = strtolower(trim($e));
     }
-
-    public function getDesc() {
-        return $this->desc;
-    }
-    public function setDesc($d) {
-        $this->desc = $d;
-    }
-
-    public function getDate() {
-        return $this->date;
-    }
-    public function setDate() {
-        $this->date = date('Y-m-d H:i:s');
-    }
-
-    public function getStatus() {
-        return $this->status;
-    }
-    public function setStatus($s) {
-        $this->status = trim($s);
-    }
-
 }
 
-interface UserDAO {
+interface UsuarioDAO {
     public function add(Usuario $u);
     public function findAll();
+    public function findByEmail($email);
     public function findByID($id);
     public function update(Usuario $u);
     public function delete($id);
