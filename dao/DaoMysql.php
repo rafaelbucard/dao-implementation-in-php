92901019edc78a52535bd39fa844261070ac072b
Rafael Pereira Buçard <36693529+rafaelbucard@users.noreply.github.com>
2021-05-29 22:48:35 -0300

    Update DaoMysql.php

--- dao/DaoMysql.php
+++ dao/DaoMysql.php
@@ -1,92 +1,64 @@
 <?php
-require_once 'models/User.php';
+require_once 'models/Usuario.php';
 
-class DaoMysql implements UserDAO {
+class UsuarioDaoMysql implements UsuarioDAO {
     private $pdo;
-
-    public function __construct(PDO $driver) {
+    public function __construct(PDO $driver){
         $this->pdo = $driver;
     }
-
-    public function add(Usuario $u) {
-        $sql = $this->pdo->prepare("INSERT INTO repair ( `namem`, `namec`, `description`, `completed`, `date`, `price`) VALUES (:namem, :namec, :descri, :comp, :dt, :price)");
-        $sql->bindValue(':namem', $u->getName());
-        $sql->bindValue(':namec', $u->getClient());
-        $sql->bindValue(':descri', $u->getDesc());
-        $sql->bindValue(':comp', $u->getStatus());
-        $sql->bindValue(':dt', $u->getDate());
-        $sql->bindValue(':price', $u->getPrice());
+    public function add(Usuario $u){
+        $sql = $this->pdo->prepare("INSERT INTO usuarios(nome, email) VALUES (:nome, :email)");
+        $sql->bindValue(':nome', $u->getNome());
+        $sql->bindValue(':email', $u->getEmail());
         $sql->execute();
 
-        $u->setId( $this->pdo->lastInsertId() );
+        $u->setId($this->pdo->lastInsertId());
         return $u;
+        
     }
-
-    public function findAll() {
+    public function findAll(){
         $array = [];
 
-        $sql = $this->pdo->query("SELECT * FROM repair");
-        if($sql->rowCount() > 0) {
+        $sql = $this->pdo->query("SELECT * FROM usuarios");
+        if($sql->rowCount()>0){
             $data = $sql->fetchAll();
 
-            foreach($data as $item) {
+            foreach($data as $item){
                 $u = new Usuario();
                 $u->setId($item['id']);
-                $u->setName($item['namem']);
-                $u->setClient($item['namec']);
-                $u->setDesc($item['description']);
-                $u->setStatus($item['completed']);
-                $u->setPrice($item['price']);
-                $u->setDate($item['date']);
+                $u->setNome($item['nome']);
+                $u->setEmail($item['email']);
 
                 $array[] = $u;
             }
         }
-
         return $array;
     }
-
-
-    public function findById($id) {
-        $sql = $this->pdo->prepare("SELECT * FROM repair WHERE id = :id");
-        $sql->bindValue(':id', $id);
+    public function findByEmail($email){
+        $sql = $this->pdo->prepare("SELECT * usuarios WHERE EMAIL = :email");
+        $sql->bindValue(':email',$email);
         $sql->execute();
-        if($sql->rowCount() > 0) {
+        if($sql->rowCount()>0) {
             $data = $sql->fetch();
 
             $u = new Usuario();
             $u->setId($data['id']);
-            $u->setName($data['namem']);
-            $u->setClient($data['namec']);
-            $u->setDesc($data['description']);
-            $u->setStatus($data['completed']);
-            $u->setPrice($data['price']);
-            $u->setDate($data['date']);
-
-
+            $u->setNome($data['nome']);
+            $u->setEmail($data['email']);
+            
             return $u;
         } else {
             return false;
         }
     }
 
-    public function update(Usuario $u) {
-        $sql = $this->pdo->prepare("UPDATE repair SET namem = :namem, namec = :namec, description = :description, completed = :comp, date = :dt, price = :price WHERE id = :id");
-        $sql->bindValue(':namem', $u->getName());
-        $sql->bindValue(':namec', $u->getClient());
-        $sql->bindValue(':descri', $u->getDesc());
-        $sql->bindValue(':comp', $u->getStatus());
-        $sql->bindValue(':dt', $u->getDate());
-        $sql->bindValue(':price', $u->getPrice());
-        $sql->bindValue(':id', $u->getId());
-        $sql->execute();
+    public function findByID($id){
 
-        return true;
     }
+    public function update(Usuario $u){
 
-    public function delete($id) {
-        $sql = $this->pdo->prepare("DELETE FROM usuarios WHERE id = :id");
-        $sql->bindValue(':id', $id);
-        $sql->execute();
+    }
+    public function delete($id){
+
     }
 }
